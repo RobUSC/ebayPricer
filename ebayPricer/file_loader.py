e699c26eaeda14d16b52c57850f39d21540d3903
Rob Carter <robertcarterusc@gmail.com>
2023-03-25 08:23:55 -0400

    File_Loader into mongodb, ObjectID add to Excel

--- ebayPricer/file_loader.py
+++ ebayPricer/file_loader.py
@@ -18,23 +18,27 @@ def ingest_excel_sheets():
 
     path_name = os.path.join("ebayPricer", "imports")
     for filename in glob.glob(os.path.join(path_name, '*.xlsx')):
-        with open(filename, 'w') as f:
+        with open(filename, 'r') as f:
             warnings.simplefilter(action='ignore', category=UserWarning)
-            df = pd.read_excel(io=filename,  sheet_name='Comics')
+            df = pd.read_excel(io=filename, sheet_name='Comics', engine='openpyxl')
+            df = df[df.filter(regex='^(?!Unnamed)').columns]
             comix = []
             for idx, row in df.iterrows():
                 comic_id = ObjectId()
-                row['ID'] = comic_id
+                df.loc[idx, 'ID'] = comic_id
                 comic = {'_id': comic_id, 'graded': row['graded'], 'Grade': row['Grade'], 'publisher': row['Publisher'],
                          'series_title': row['Series Title'], 'issue_number': str(row['Issue Number']),
                          'Professional_Grader': row['Professional Grader'], 'Variant': row['Variant']}
                 comix.append(comic)
 
-            df.to_excel(filename, sheet_name='Comics')
-            data_objects.add_item_list_records(comix)
             file_import = {'hash': md5(filename), 'filename': filename}
-            data_objects.add_file_import(file_import)
-            loaded.append(file_import)
+            if len(comix) > 0:
+                df.to_excel(filename, sheet_name='Comics', index=False)
+                data_objects.add_item_list_records(comix)
+                data_objects.add_file_import(file_import)
+                loaded.append(file_import)
+            else:
+                ignored.append(file_import)
 
     files['loaded'] = loaded
     files['ignored'] = ignored
@@ -49,5 +53,3 @@ def md5(fname):
     return hash_md5.hexdigest()
 
 # def update_excel_sheets():
-
-
